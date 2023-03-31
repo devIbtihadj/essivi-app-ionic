56f255a10f05ae2e2d152f3e3a9ecf3008fdd123
ibtihadj <ibtihadj.dev@gmail.com>
2023-03-31 16:14:31 +0000

    Last modif before presentation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  //backendHost : "http://127.0.0.1:5000/"
+  backendHost : "http://127.0.0.1:5000/"
   //backendHost : "http://192.168.1.66:5000/"
   //backendHost : "http://10.3.139.214:5000/"
-  backendHost : "http://192.168.1.76:5000/"
+  //backendHost : "http://192.168.1.76:5000/"
  
 };
 
